fed8534bb5b47124f7d16be625f1255c485a7e47
Quvonchbek <1.quvonchbek.ibragimov@gmail.com>
2024-07-29 05:40:06 +0500

    update

--- config/urls.py
+++ config/urls.py
@@ -18,30 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from user.views import SuperUserLoginApiView
 
-from rest_framework import permissions
-from drf_yasg.views import get_schema_view
-from drf_yasg import openapi
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Your Project API",
-        default_version='v1',
-        description="API documentation for your project",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@yourproject.local"),
-        license=openapi.License(name="BSD License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
-
 urlpatterns = [
     path('api/v1/calls/', include('calls.urls')),
     path('superuser-login/', SuperUserLoginApiView.as_view()),
     path('', admin.site.urls),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger.yaml', schema_view.without_ui(cache_timeout=0, renderer_classes=[openapi.renderers.OpenAPIRenderer]),
-         name='schema-yaml'),
 ]
